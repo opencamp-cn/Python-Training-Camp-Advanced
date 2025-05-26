bf2c529e23ecf36d784274f6e92073a48426359e
NewAlice <819428005@qq.com>
2025-05-26 23:28:51 +0800

    update nms and smooth_l1

--- exercises/smooth_l1.py
+++ exercises/smooth_l1.py
@@ -30,4 +30,16 @@ def smooth_l1(x, sigma=1.0):
     # 3. 对满足条件的元素应用第一个公式 (0.5 * (sigma * x)**2)。
     # 4. 对不满足条件的元素应用第二个公式 (|x| - 0.5 / sigma2)。
     # 5. 可以使用 np.where() 来根据条件选择应用哪个公式。
-    pass 
+    sigma2 = sigma ** 2
+    abs_x = np.abs(x)
+    # if abs_x < 1.0 / sigma2:
+    #     return 0.5 * (sigma * x)**2
+    # return abs_x - 0.5 / sigma**2
+
+    condition = abs_x < 1.0 / sigma2
+
+    return np.where(
+        condition,
+        0.5 * sigma2 * x ** 2,
+        abs_x - 0.5 / sigma2
+    )
